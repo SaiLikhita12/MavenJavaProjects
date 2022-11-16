8d383076f2505f71b21c5e0a6faf3ac24b7b098b
SaiLikhita12 <sailikhitarage.12@gmail.com>
2022-11-16 21:46:33 +0530

    First commit to maven java project

---
+++ src/main/java/SEGroup/First_MavenJava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.First_MavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is first Maven java project by Likhita!!" );
+    }
+}
